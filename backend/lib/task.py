81ec829054ea94b12cf35253ec0ce21abd7843f8
Harivind Premkumar <50073833+Harivind@users.noreply.github.com>
2021-05-31 00:47:03 +0530

    enhanced task, taskService

--- backend/lib/task.py
+++ backend/lib/task.py
@@ -9,18 +9,22 @@ from Crypto.Hash import SHA
 import time
 
 class Task:
-    def __init__(self, resourceURL: str, threshold: float, signature:str):
-        self.date = int(time.time())
+    def __init__(self, resourceURL: str, threshold: float, maxEpochs: int, publicKey: str, signature:str):
+        self.timestamp = int(time.time())
         self.resourceURL = resourceURL
         self.threshold = round(threshold,2)
+        self.maxEpochs = maxEpochs
+        self.publicKey = publicKey
         self.signature = signature
-        assert threshold >50 and threshold <100
+        assert threshold >50 and threshold <80
 
     def toDict(self) -> OrderedDict:
         return OrderedDict({
-            "date": self.date,
+            "timestamp": self.timestamp,
             "resourceURL": self.resourceURL,
             "threshold": self.threshold,
+            "maxEpochs": self.maxEpochs,
+            "publicKey": self.publicKey,
             "signature": self.signature
         })
 
@@ -29,7 +33,7 @@ class Task:
 
     def __eq__(self, o: object) -> bool:
         if (isinstance(o, Task)):
-            if(self.date == o.date and self.resourceURL == o.resourceURL and self.threshold == o.threshold and self.signature == o.signature):
+            if(self.timestamp == o.timestamp and self.resourceURL == o.resourceURL and self.threshold == o.threshold and self.maxEpochs == o.maxEpochs and self.publicKey == o.publicKey and self.signature == o.signature):
                 return True
         return False
 
@@ -38,34 +42,44 @@ class Task:
     
     @classmethod
     def fromDict(cls, dict: Dict) -> Task:
-        return cls(dict["date"], dict["resourceURL"], dict["threshold"], dict["signature"])
+        return cls(dict["timestamp"], dict["resourceURL"], dict["threshold"], dict["maxEpochs"], dict["publicKey"], dict["signature"])
 
 class TaskSolution:
-    def __init__(self, task: Task, modelURL: str, accuracy: float, signature:str):
-        self.date = int(time.time())
-        self.taskHash = task.getHash()
+    def __init__(self, task: Task, modelURL: str, accuracy: float, publicKey: str, signature:str):
+        self.timestamp = int(time.time())
+        self.taskId = task.getHash()
         self.modelURL = modelURL
         self.accuracy = round(accuracy,2)
+        self.wst = 0
+        self.publicKey = publicKey
         self.signature = signature
 
     def toDict(self) -> OrderedDict:
         return OrderedDict({
-            "date": self.date,
-            "taskHash": self.taskHash,
+            "timestamp": self.timestamp,
+            "taskId": self.taskId,
             "modelURL": self.modelURL,
             "accuracy": self.accuracy,
+            "wst": self.wst,
+            "publicKey": self.publicKey,
             "signature": self.signature
         })
 
     def __eq__(self, o: object) -> bool:
         if (isinstance(o, TaskSolution)):
-            if(self.date == o.date and self.taskHash == o.taskHash and self.modelURL == o.modelURL and self.accuracy == o.accuracy and self.signature == o.signature):
+            if(self.timestamp == o.timestamp and self.taskId == o.taskId and self.modelURL == o.modelURL and self.accuracy == o.accuracy and self.wst == o.wst and self.publicKey == o.publicKey and self.signature == o.signature):
                 return True
         return False
 
     def __str__(self) -> str:
         return json.dumps(self, default=lambda o: o.toDict())
     
+    def getUnsignedStr(self) -> str:
+        dict = json.loads(str(self))
+        print(dict)
+        return json.dumps(dict, default=lambda o: o.toDict())
+
     @classmethod
     def fromDict(cls, dict: Dict) -> TaskSolution:
-        return cls(dict["date"], dict["taskHash"], dict["modelURL"], dict["accuracy"], dict["signature"])
+        return cls(dict["timestamp"], dict["taskId"], dict["modelURL"], dict["accuracy"], dict["wst"], dict["publicKey"], dict["signature"])
+        
--- backend/lib/taskService.py
+++ backend/lib/taskService.py
@@ -1,13 +1,16 @@
 import tensorflow as tf
 from tensorflow.keras.utils import to_categorical
-from tensorflow.keras.callbacks import ModelCheckpoint
+from tensorflow.keras.callbacks import ModelCheckpoint, EarlyStopping
 from tensorflow.keras.optimizers import SGD, Adam
 from task import Task, TaskSolution
 import pandas as pd
 import numpy as np
 from pyunpack import Archive
 from pathlib import Path
+import requests
 import wget
+from utils import generateSignature
+import traceback
 
 # Task:
 #     |
@@ -19,30 +22,65 @@ import wget
 #     |
 #     |---config.json
 
+thresholdReached = False
+
+class EarlyStoppingByValAcc(tf.keras.callbacks.Callback):
+    global thresholdReached
+    def __init__(self, monitor='val_accuracy', value = 0.8):
+        super(tf.keras.callbacks.Callback, self).__init__()
+        self.monitor = monitor
+        self.value = value
+
+    def on_epoch_end(self, epoch, logs={}): 
+        val_accuracy = logs.get(self.monitor)
+        if(val_accuracy >= self.value): 
+            print("Threshold acc reached, stopping training ...")
+            self.model.stop_training = True
+            thresholdReached = True
+
 
 class TaskService:
     
     downloadFolder = "../../downloads"
     solutionFolder = "../../solutions"
     taskFolder = "../../currentTask"
-    uploadCmd = "curl  --upload-file {fileLoc} https://transfer.sh/task" 
 
     @staticmethod
-    def createTask(task: Task):
-        return ""
-    
+    def uploadTaskSolution():
+        url = "https://transfer.sh/solution"
+
+        payload = Path(TaskService.solutionFolder + "/solution.h5").read_bytes()
+        headers = {
+            'Content-Type': 'application/octet-stream'
+        }
+        print("Uploading TaskSolution ...")
+        response = requests.request("PUT", url, headers=headers, data=payload)
+        print("TaskSolution Uploaded ...")
+        return response.text
+
     @staticmethod
-    def downloadTask(taskURL: str) -> str:
+    def downloadTask(task: Task) -> str:
+        # Alternate using requests module
+        # payload={}
+        # headers = {}
+        # response = requests.request("GET", task.resourceURL, headers=headers, data=payload)
+        # Path(TaskService.downloadFolder +"/" + task.resourceURL.split('/')[-1]).write_bytes(response.content)
+
         Path(TaskService.downloadFolder).mkdir(parents=True, exist_ok=True)
         Path(TaskService.taskFolder).mkdir(parents=True, exist_ok=True)
-        file = wget.download(taskURL,out=TaskService.downloadFolder +"/" + taskURL.split('/')[-1] )       
+        print("Downloading Task ...")
+        file = wget.download(task.resourceURL, out=TaskService.downloadFolder +"/" + task.resourceURL.split('/')[-1])
+        print("Download completed ...")
         #unzip to taskFolder
+        print("Extracting the file ...")
         Archive(file).extractall(TaskService.taskFolder)
         return file
 
     @staticmethod
-    def mine(task: Task):
+    def runTask(task: Task, privateKey: str) -> TaskSolution:
         try:
+            global thresholdReached 
+            thresholdReached = False
             #read data
             train = pd.read_csv('../../currentTask/data/train.csv',  header=None)   
             test = pd.read_csv('../../currentTask/data/test.csv',  header=None)   
@@ -65,13 +103,17 @@ class TaskService:
             #create solutions folder if not present
             Path(TaskService.solutionFolder).mkdir(parents=True, exist_ok=True)
 
-            #callback to save best acc mdodel
-            checkpointer = ModelCheckpoint(
-                filepath= TaskService.solutionFolder + '/solution.hdf5', 
+            #callbacks to save best acc mdodel, stop on reaching threshold
+            checkpointer1 = ModelCheckpoint(
+                filepath= TaskService.solutionFolder + '/solution.h5', 
                 verbose=1, 
                 save_best_only=True,
                 save_weights_only=True, 
                 monitor='val_accuracy'
+            )
+            checkpointer2 = EarlyStoppingByValAcc(
+                 monitor='val_accuracy',
+                 value=task.threshold / 100,
             )
 
             mlModel.compile(
@@ -80,29 +122,37 @@ class TaskService:
                 metrics=['accuracy']
             )
 
-            print("Starting to Mine...")
+            print("Starting to train...")
             history = mlModel.fit(
                 xTrain,
                 yTrainCat,
                 batch_size=32,
-                epochs=1,
-                validation_data=(xTest,yTestCat),
+                epochs=10,
+                validation_data=(xTest, yTestCat),
                 verbose=1,
-                callbacks=[checkpointer]
+                callbacks=[checkpointer1, checkpointer2]
             )
-            print("Completed Mining..", history.history)
-
-            # return TaskSolution(
-            #     task,
-            #     modelURL,
-            #     history.val_accuracy
-            #     signature
-            # )
+            print("Completed training...", history.history)
+            
+            taskSolutionURL = TaskService.uploadTaskSolution()
+            print(taskSolutionURL)
+            taskSolution = TaskSolution(
+                task,
+                taskSolutionURL,
+                history.history["val_accuracy"][-1],
+                1 if thresholdReached else 0.1,
+                ""
+            )
+            #sign the taskSolution
+            taskSolution.signature = generateSignature(taskSolution.getUnsignedStr(),privateKey)
+            print(taskSolution.toDict())
+            return taskSolution
         except Exception as e:
-            print("Error occured while mining..\n", e)
+            print("Error occured while training..\n", e.__repr__)
+            traceback.print_exc()
   
     @staticmethod
-    def verifyFiles() -> bool:
+    def __verifyTaskFiles() -> bool:
         try:
             mlModel = tf.keras.models.load_model(TaskService.taskFolder+'/model')
             train = pd.read_csv('../../currentTask/data/train.csv',  header=None)   
@@ -110,8 +160,15 @@ class TaskService:
             return True
         except:
             print("File verification unsuccessful")
-            return False
+            return False  
+
+    @staticmethod
+    def verifyTask(task: Task) -> bool:
+        TaskService.downloadTask(task)
+        TaskService.__verifyTaskFiles()
 
 if __name__ == "__main__":
-    TaskService.downloadTask("https://transfer.sh/jX/poker.zip")
-    TaskService.mine()
+    taskk = Task("https://transfer.sh/1dfkQCG/poker.zip",51,10,"","")
+    TaskService.downloadTask(taskk)
+    xx = TaskService.runTask(taskk,"3082025b02010002818100cb8d1b8ae3f9e568284f181f3c5fc2cf98c2e13a9f21734fa41a33bed6745e93f95c6f44e27a5f9992981c3d1bd613166f8bbf9a2245f576deb91049354635887c62eeb22969ef63a7b5fc2c53701b067dcc9425e11dac183f3328c4b64dd6493b454b09b9d24e228acd8f8795ce673b4804549a4c9de6dca9511252bb5e975302030100010281800b95c2658b4823c863d2ec9a8a8320c153fe737734ab60b6bdae4817aa79011106f63dd6fbd4df300c69dfe1927e02a41a4127ec8bdad377aa8179edd0bc683b7c491d7f26016600750725bcc2b3138ac3c84c8eb47fa59bc30c517ca212d6ba0d841ceb7baed795a9a007767a8d59faeb68c28367f82925593a2b6f04c23561024100cdd1a3829426ab3c8d765c996d44cc90140150cf9972f408025f70b3707d1e625c9e25fde646641752633f55d25379574f10cc3bf73ebb42b4d9c95b62867d39024100fd2dedb0a58f4f8e416574c0da602318fb5c0e02569ac9f2d157bb4bea4ba73b45740d53b6496409c3c2c658558ad0c58a9ca624c940e537c8d93f46ac5ac4eb024007d6b4238500f4049a5ea7a830412e894e39be9a297df74d56c9cbc109c7ba2084e6810bea7943d69f8ca81cdca5d1394209a1bda6ecfcb4cdae7dbcbd43e20102400ecdb8b0337e05b0d3b212f993cb3b4222b067414bbf113fd96dbfcdd88d43e1fb55a5d1d73ec352aed79cb15d8f1855f49ce43a126a70fcaa09c9e160028eb10240686c70c1cad0a791c41dfbc226c19436f9ccc18bab480bfbbc0322782dff73a2248acfedddaba4f1cf22fa8fa34815803e80b2e9b2f94f63695b0d8217d983e8")
+    
