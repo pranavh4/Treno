1c728a5e4dbb9c729c98f90d3b23b357a22d5b9e
pranavh4 <pranavh4@gmail.com>
2021-06-05 19:14:16 +0530

    Add proxy

--- backend/blockchain_client.py
+++ backend/blockchain_client.py
@@ -2,7 +2,7 @@ from lib.block_explorer import BlockExplorer
 from lib.utils import generateSignature
 from lib.task import Task
 import requests
-from flask import Flask, jsonify, request, render_template
+from flask import Flask, jsonify, request, render_template, Response
 from flask_cors import CORS
 import json
 import sys
@@ -49,11 +49,20 @@ def generateTask():
     print(task)
     return requests.post('http://localhost:5000/tasks/add', json=json.loads(str(task))).json()
 
-@app.route('/get/blocks')
-def getBlocks():
-    endHeight = request.args.get("endHeight", -1)
-    numBlocks = request.args.get("numBlocks", 10)
-    return jsonify(BlockExplorer.getBlocks(endHeight, numBlocks))
+
+@app.route('/<path:path>',methods=['GET', 'POST'])
+def proxy(path):
+    if request.method=='GET':
+        resp = requests.get(f'http://localhost:5000/{path}')
+        excluded_headers = ['content-encoding', 'content-length', 'transfer-encoding', 'connection']
+        headers = [(name, value) for (name, value) in     resp.raw.headers.items() if name.lower() not in excluded_headers]
+        response = Response(resp.content, resp.status_code, headers)
+    if request.method=='POST':
+        resp = requests.post(f'http://localhost:5000/{path}',json=request.get_json())
+        excluded_headers = ['content-encoding', 'content-length', 'transfer-encoding', 'connection']
+        headers = [(name, value) for (name, value) in resp.raw.headers.items() if name.lower() not in excluded_headers]
+        response = Response(resp.content, resp.status_code, headers)
+    return response
 
 if __name__ == "__main__":
     app.run(host='127.0.0.1', port=8000, debug=True)
